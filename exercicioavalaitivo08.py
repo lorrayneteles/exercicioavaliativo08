d247a20027e7de2c25faa773ced463dd0436ee3f
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-04 21:28:28 -0300

    Add files via upload

---
+++ exercicioavalaitivo08.py
@@ -0,0 +1,7 @@
+# converção de temperatura
+
+c = int(input("Digite a temperatura em Celsius:\n"))
+
+temperatura_fahrenheit =(9*c + 160)/5
+
+print("A temperatura na escala fahrenheit é:\n", temperatura_fahrenheit)
